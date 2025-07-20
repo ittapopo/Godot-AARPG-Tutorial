3d71c11214fb4e935c7ed507e37fe22636165094
Stian Selle <stianstian95@gmail.com>
2025-07-20 21:21:01 +0200

    slime enemy and slime enemy state machine

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -12,23 +12,20 @@ signal DirectionChanged( new_direction: Vector2 )
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	PlayerManager.player = self
 	state_machine.Initialize(self)
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process( delta ):
-	
-	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
+func _process( _delta ):
 	direction = Vector2(
 		Input.get_axis("left", "right"),
 		Input.get_axis("up", "down")
 	).normalized()
 	pass
 
-func _physics_process(delta: float):
+func _physics_process( _delta: float):
 	move_and_slide()
 
 
