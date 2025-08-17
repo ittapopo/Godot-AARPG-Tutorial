8430fad043adcecd01f4f6476bd992e52c86cf24
Stian Selle <stianstian95@gmail.com>
2025-08-17 11:53:32 +0200

    player spawn WIP
    working on adding player spawn aswell as a new level

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -1,4 +1,20 @@
 extends Node
 
+const PLAYER = preload("res://Player/player.tscn")
 
 var player : Player
+var player_spawned : bool = false
+
+
+func _ready() -> void:
+	add_player_instance()
+
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child( player )
+	pass
+
+
+func set_player_position( _new_pos : Vector2 ) -> void:
+	player.global_position = _new_pos
+	pass
